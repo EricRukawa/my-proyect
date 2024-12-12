f2328a48ed5c5412b9be4a11e5f1cbf7bf2750ce
Eric/Rukaw@ <fuentesbonanza@gmail.com>
2024-12-11 21:48:16 -0300

    cambios

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Table from './components/data-table'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+
+      </div>
+      <p className="read-the-docs">
+        Listado de Vendedores
+      </p>
+
+      <Table />
+    </>
+
+
+  )
+}
+
+export default App
+
+
